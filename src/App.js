49a1fd3c6e6512e6386fcbe64b28ea2bd9c274cb
dumb-programmer <asadkhan6192@gmail.com>
2022-03-20 12:12:05 +0500

    Implement basic functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+import { useState } from "react";
+import Cards from "./components/Cards";
 
 function App() {
-  return <div className="App"></div>;
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const [selected, setSelected] = useState([]);
+  const [win, setWin] = useState(false);
+
+  if (!win) {
+    return (
+      <div className="App">
+        <div className="scoreboard">
+          Score : {score} High Score : {highScore}
+        </div>
+        <Cards
+          items={items}
+          setItems={setItems}
+          selected={selected}
+          setSelected={setSelected}
+          score={score}
+          setScore={setScore}
+          highScore={highScore}
+          setHighScore={setHighScore}
+          setWin={setWin}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>You Won!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
