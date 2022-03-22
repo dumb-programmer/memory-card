32882536619ca0876fbb12437f6dede1e5a025ef
dumb-programmer <asadkhan6192@gmail.com>
2022-03-22 11:35:38 +0500

    Use Kung Fu Panda Characters instead of placeholder images

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import "./styles/App.css";
 import { useState } from "react";
 import CardsContainer from "./components/CardsContainer";
 import uniqid from "uniqid";
-import cat1 from "./images/cat1.jpeg";
-import cat2 from "./images/cat2.jpeg";
-import cat3 from "./images/cat3.jpeg";
-import cat4 from "./images/cat4.jpeg";
-import cat5 from "./images/cat5.jpeg";
 import ScoreBoard from "./components/ScoreBoard";
+import Po from "./assets/images/Po.jpeg";
+import MrPing from "./assets/images/MrPing.jpeg";
+import Shifu from "./assets/images/Shifu.jpeg";
+import Tigress from "./assets/images/Tigress.jpeg";
+import Monkey from "./assets/images/Monkey.jpeg";
+import Mantis from "./assets/images/Mantis.jpeg";
+import Kai from "./assets/images/Kai.jpeg";
+import Oogway from "./assets/images/Oogway.jpeg";
+import Tai_Lung from "./assets/images/Tai_Lung.jpeg";
+import Lord_Shen from "./assets/images/Lord_Shen.jpeg";
 
 const App = () => {
   const unchangedItems = [
-    { id: uniqid(), number: 1, name: "Cat 1", path: cat1 },
-    { id: uniqid(), number: 2, name: "Cat 2", path: cat2 },
-    { id: uniqid(), number: 3, name: "Cat 3", path: cat3 },
-    { id: uniqid(), number: 4, name: "Cat 4", path: cat4 },
-    { id: uniqid(), number: 5, name: "Cat 5", path: cat5 },
+    { id: uniqid(), number: 1, name: "Po", path: Po },
+    { id: uniqid(), number: 2, name: "Mr Ping", path: MrPing },
+    { id: uniqid(), number: 3, name: "Shifu", path: Shifu },
+    { id: uniqid(), number: 4, name: "Tigress", path: Tigress },
+    { id: uniqid(), number: 5, name: "Monkey", path: Monkey },
+    { id: uniqid(), number: 6, name: "Mantis", path: Mantis },
+    { id: uniqid(), number: 7, name: "Kai", path: Kai },
+    { id: uniqid(), number: 8, name: "Oogway", path: Oogway },
+    { id: uniqid(), number: 9, name: "Tai Lung", path: Tai_Lung },
+    { id: uniqid(), number: 10, name: "Lord Shen", path: Lord_Shen },
   ];
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
